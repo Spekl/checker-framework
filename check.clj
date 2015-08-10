a5777b5efeee5e71bab3021fe768f53032d856e1
John L. Singleton <jsinglet@gmail.com>
2015-08-10 17:41:52 -0400

    Updates to checker-framework version 1.9.4-2

--- check.clj
+++ check.clj
@@ -2,15 +2,7 @@
  (:require [clojure.java.io :as io]
             [clojure.tools.logging :as log]))
 
-;;
-;; All checks must at least define a "default" check and can optionally define any number of named checks.
-;;
-(defcheck default
-  ;; Printing a log message...
-  (log/info  "A message")
 
-  ;; running a check
-  (log/info  "Running a check.")  
-  ;(run "cmd" "arg1" "${installed-package:asset.jar}" "-esc" *project-files-string* )
-  )
+(defcheck default
+  (log/info  "This package does not provide any checks. Please use one of the checker-framework-* packages to install checks."))
 
